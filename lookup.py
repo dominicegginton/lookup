37b093686db8f88e75f4b5e0dcbf6a57e0827863
Dominic Egginton <dominic.egginton@gmail.com>
2017-05-19 17:40:18 +0100

    added argsparser

--- lookup.py
+++ lookup.py
@@ -1,10 +1,20 @@
 # File Name: lookup.py
 # Description: use this small handy program to lookup any ip adress. lookup uses the api focurl und at http://ip-api.com/
 # https://github.com/dominicegginton/lookup
-import requests,sys
+import requests,sys,argparse
 
 def Main():
-    r = requests.get('http://ip-api.com/json/'+sys.argv[1])
+    parser = argparse.ArgumentParser(description='Lookup by Dominic Egginton')
+    parser.add_argument('-a', '--address', metavar='lookup', type=str, help='lookup adrress')
+    args = parser.parse_args()
+    
+    
+    
+    if args.address:
+        print('doing ' + args.address)
+        r = requests.get('http://ip-api.com/json/'+args.address)
+    else:
+        r = requests.get('http://ip-api.com/json/')
     requestJSON = r.json() 
     for key, value in requestJSON.items():
         print(str(key)+': '+str(value))
