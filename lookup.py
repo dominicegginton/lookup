3ed60737bc575f580f2fc6543907aadbbf33fe12
Dominic Egginton <dominic.egginton@gmail.com>
2017-05-19 18:24:15 +0100

    fixed assii banner art

--- lookup.py
+++ lookup.py
@@ -12,6 +12,14 @@ def Main():
     parser.add_argument('-a', '--address', metavar='lookup', type=str, help='lookup adrress')
     args = parser.parse_args()
 
+    # print header
+    print(" _     ___   ___  _  ___   _ ____  ")
+    print("| |   / _ \ / _ \| |/ / | | |  _ \ ")
+    print("| |  | | | | | | | ' /| | | | |_) |")
+    print("| |__| |_| | |_| | . \| |_| |  __/ ")
+    print("|_____\___/ \___/|_|\_\\\___/|_|    ")
+    print("")
+
     # if address is supplied
     if args.address:
         r = requests.get('http://ip-api.com/json/'+args.address)
