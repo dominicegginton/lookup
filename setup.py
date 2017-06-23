f9d9cf96f440b14181a76e0e2c21466cf3e8b127
Dominic Egginton <dominic.egginton@gmail.com>
2017-06-23 15:07:08 +0100

    added long des to setup.py file

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 """setup"""
+import os
 from setuptools import setup
+
+def read_file(path):
+    with open(os.path.join(os.path.dirname(__file__), path)) as fp:
+        return fp.read()
 
 setup(
     name='ip-lookup',
-    version='1.2',
+    version='1.3',
     py_modules=['lookup'],
     platforms='any',
     license='MIT',
@@ -11,6 +16,8 @@ setup(
     url='https://github.com/dominicegginton/lookup',
     author_email='dominic.egginton@gmail.com',
     description='IP lookup via the command line',
+    long_description=read_file('README.md'),
+    keywords='ip lookup ping tracerout',
     entry_points={
         'console_scripts': [
             'lookup = lookup:main',
